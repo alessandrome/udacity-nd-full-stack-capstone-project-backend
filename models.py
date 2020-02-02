5eee2734dcdb9e79e353bd303e03b4900fdef119
alessandrome <mesti.alessandro.git@gmail.com>
2020-02-02 15:31:40 +0100

    Make abstract class to add insert, update and delete methods to db.Model class

--- models.py
+++ models.py
@@ -18,10 +18,23 @@ def setup_db(app, database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.create_all()
 
 
-class User(db.Model):
+class ModelAction(db.Model):
+    __abstract__ = True
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class User(ModelAction):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     auth0_id = db.Column(db.String(64), unique=True)
@@ -31,40 +44,53 @@ class User(db.Model):
     tournaments = db.relationship('Tournament', backref=db.backref('tournaments', cascade="all, delete-orphan"))
 
 
-class Game(db.Model):
+class Game(ModelAction):
     __tablename__ = 'games'
     id = Column(db.Integer, primary_key=True)
     name = Column(db.String)
 
 
-class Match(db.Model):
+class Match(ModelAction):
     __tablename__ = 'matches'
     id = Column(db.Integer, primary_key=True)
     name = Column(db.String)
     uuid = Column(db.String(64))
     creator_id = db.Column(db.Integer)
     game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+    is_private = db.Column(db.Boolean, default=False)
     tournament_id = db.Column(db.Integer, db.ForeignKey('tournaments.id'), nullable=True)
     max_participants = db.Column(db.Integer, nullable=True)
     created_at = db.Column(db.DateTime(), default=datetime.now)
     updated_at = db.Column(db.DateTime(), default=datetime.now, onupdate=datetime.now)
+    creator = db.relationship('User', backref='created_tournaments')
     match_participations = db.relationship('MatchParticipants', backref='match')
     users = db.relationship('User', backref=db.backref('users', cascade="all, delete-orphan"))
 
+    def short(self):
+        match = {
+            'id': self.id,
+            'name': self.name,
+            'game': self.game,
+            'max_participants': self.max_participants,
+        }
+        return match
 
-class Tournament(db.Model):
+
+class Tournament(ModelAction):
     __tablename__ = 'tournaments'
     id = Column(db.Integer, primary_key=True)
     name = Column(db.String)
     uuid = Column(db.String(64))
+    creator_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
     tournament_id = db.Column(db.Integer, db.ForeignKey('tournaments.id'))
     start_date = db.Column(db.DateTime, nullable=True)
+    creator = db.relationship('User', backref='tournaments')
     tournament_participations = db.relationship('TournamentParticipants', backref='tournament')
     users = db.relationship('User', backref=db.backref('users', cascade="all, delete-orphan"))
 
 
-class TournamentParticipants(db.Model):
+class TournamentParticipants(ModelAction):
     __tablename__ = 'tournament_participants'
     id = db.Column(db.Integer, primary_key=True)
     tournament_id = db.Column(db.Integer, db.ForeignKey('tournaments.id'))
@@ -72,7 +98,7 @@ class TournamentParticipants(db.Model):
     participate_date = db.Column(db.DateTime(), default=datetime.now)
 
 
-class MatchParticipants(db.Model):
+class MatchParticipants(ModelAction):
     __tablename__ = 'match_participants'
     id = db.Column(db.Integer, primary_key=True)
     match_id = db.Column(db.Integer, db.ForeignKey('matches.id'))
